d7d54624cf851f2927131d88bf2b0313d0cebf50
Mauricio Robayo <rfmajo@gmail.com>
2020-12-29 18:11:57 -0500

    feat: add mega-banner to layout
    Signed-off-by: Mauricio Robayo <rfmajo@gmail.com>

---
+++ components/the-moneytizer/mega-banner.tsx
@@ -0,0 +1,15 @@
+const isProduction = process.env.NODE_ENV === "production";
+const id = "71116-1";
+
+export default () => (
+  <div id={id}>
+    {isProduction ? (
+      <>
+        <script src="//ads.themoneytizer.com/s/gen.js?type=1" />
+        <script src="//ads.themoneytizer.com/s/requestform.js?siteId=71116&formatId=1" />
+      </>
+    ) : (
+      <div>Ad placeholder</div>
+    )}
+  </div>
+);
