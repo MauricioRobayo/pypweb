849df383cb2d0d2296effe6a209ba4f7128830bb
Mauricio Robayo <rfmajo@gmail.com>
2020-12-28 20:13:20 -0500

    feat: add gtag on production
    Signed-off-by: Mauricio Robayo <rfmajo@gmail.com>

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,29 @@
 import { AppProps } from 'next/app';
-import '../styles/reset.css';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+import * as gtag from '../lib/gtag';
 import '../styles/globals.scss';
+import '../styles/reset.css';
 
-function App({ Component, pageProps }: AppProps) {
+const isProduction = process.env.NODE_ENV === 'production';
+
+const App = ({ Component, pageProps }: AppProps) => {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url: URL) => {
+      /* invoke analytics function only for production */
+      if (isProduction) {
+        gtag.pageview(url);
+      }
+    };
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
   return <Component {...pageProps} />;
-}
+};
 
 export default App;
