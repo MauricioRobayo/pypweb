7b3b1696dc6ef2d1faf403a264c89eb1676bc517
Mauricio Robayo <rfmajo@gmail.com>
2021-01-03 10:58:13 -0500

    fix: use named function for MegaBanner component
    Signed-off-by: Mauricio Robayo <rfmajo@gmail.com>

--- components/the-moneytizer/mega-banner.tsx
+++ components/the-moneytizer/mega-banner.tsx
@@ -1,17 +1,25 @@
 import styles from "./mega-banner.module.scss";
 
 const isProduction = process.env.NODE_ENV === "production";
-const id = "71116-1";
+const siteId = "71116";
+const formatId = "1";
+const type = "1";
 
-export default () => (
-  <div id={id}>
-    {isProduction ? (
-      <>
-        <script src="//ads.themoneytizer.com/s/gen.js?type=1" />
-        <script src="//ads.themoneytizer.com/s/requestform.js?siteId=71116&formatId=1" />
-      </>
-    ) : (
-      <div className={styles.megaBanner}>Mega Banner</div>
-    )}
-  </div>
-);
+function MegaBanner() {
+  return (
+    <div id={siteId}>
+      {isProduction ? (
+        <>
+          <script src={`//ads.themoneytizer.com/s/gen.js?type=${type}`} />
+          <script
+            src={`//ads.themoneytizer.com/s/requestform.js?siteId=${siteId}&formatId=${formatId}`}
+          />
+        </>
+      ) : (
+        <div className={styles.megaBanner}>Mega Banner</div>
+      )}
+    </div>
+  );
+}
+
+export default MegaBanner;
