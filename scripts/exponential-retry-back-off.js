ba926cffc3830eac9d26fd1560ad0bc54c1f3705
Mauricio Robayo <rfmajo@gmail.com>
2021-02-17 12:34:17 -0500

    chore: update exponential-retry-backoff
    Signed-off-by: Mauricio Robayo <rfmajo@gmail.com>

--- scripts/exponential-retry-back-off.js
+++ scripts/exponential-retry-back-off.js
@@ -3,18 +3,13 @@
 const axios = require("axios").default;
 
 const maxTimeInHours = 8;
-const maxTimeInMinutes = maxTimeInHours * 60;
-const maxTimeInSeconds = maxTimeInMinutes * 60;
-
 const webhook =
   "https://api.vercel.com/v1/integrations/deploy/prj_MQlxIz5zQP4GMibWntBaE9r3ws4B/zrVrtysmCF";
-
-let lapseInSeconds = 2;
 const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
+let lapseInSeconds = 2;
 const exponentialRetryBackOff = async () => {
-  while (lapseInSeconds < maxTimeInSeconds) {
-    lapseInSeconds *= 2;
+  while (lapseInSeconds < maxTimeInHours * 60 * 60) {
     try {
       const { data, status, statusText } = await axios.post(webhook);
 
@@ -27,7 +22,9 @@ const exponentialRetryBackOff = async () => {
       console.log(error);
       return 1;
     }
+
     await sleep(lapseInSeconds * 1000);
+    lapseInSeconds *= 2;
   }
 
   return 1;
