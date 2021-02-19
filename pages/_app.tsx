615afb9bad26581be1ff0fbf960b3243146b95a4
Mauricio Robayo <rfmajo@gmail.com>
2021-02-19 06:54:42 -0500

    chore: move css globals/reset to styled-components
    Signed-off-by: Mauricio Robayo <rfmajo@gmail.com>

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,8 +2,55 @@ import * as gtag from "lib/gtag";
 import { AppProps } from "next/app";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
-// organize-imports-ignore
-import "../styles/globals.scss";
+import { createGlobalStyle } from "styled-components";
+import CssReset from "styles/css-reset";
+
+const GlobalStyle = createGlobalStyle`
+  ${CssReset};
+  @font-face {
+    font-family: "LicensePlate";
+    font-style: normal;
+    font-weight: normal;
+    src: url("/fonts/LicensePlate.eot");
+    src: url("/fonts/LicensePlate.eot?#iefix") format("embedded-opentype"),
+      url("/fonts/LicensePlate.otf") format("opentype"),
+      url("/fonts/LicensePlate.svg") format("svg"),
+      url("/fonts/LicensePlate.ttf") format("truetype"),
+      url("/fonts/LicensePlate.woff") format("woff"),
+      url("/fonts/LicensePlate.woff2") format("woff2");
+  }
+
+  * {
+    box-sizing: border-box;
+  }
+
+  :root {
+    --max-width: 570px;
+    --active-background-color: hsl(51, 100%, 92%);
+    --active-color: hsl(51, 100%, 56%);
+    --inactive-background-color: hsl(0, 0%, 96%);
+    --link-color: hsl(217, 71%, 53%);
+  }
+
+  body {
+    font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+      Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+    line-height: normal;
+  }
+
+  strong {
+    font-weight: bold;
+  }
+
+  a {
+    color: inherit;
+    text-decoration: none;
+
+    &:hover {
+      color: var(--link-color);
+    }
+  }
+`;
 
 const isProduction = process.env.NODE_ENV === "production";
 
@@ -22,7 +69,12 @@ const App = ({ Component, pageProps }: AppProps) => {
     };
   }, [router.events]);
 
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </>
+  );
 };
 
 export default App;
