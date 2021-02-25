ca7cc12893e2a68f0e0724891e3ada6bf8b40f62
Mauricio Robayo <rfmajo@gmail.com>
2021-02-25 10:59:45 -0500

    fix(#70): no need to check status in Axios
    Signed-off-by: Mauricio Robayo <rfmajo@gmail.com>

--- scripts/exponential-retry-back-off.js
+++ scripts/exponential-retry-back-off.js
@@ -3,30 +3,32 @@
 const axios = require("axios").default;
 
 const maxTimeInHours = 8;
+const timeoutMs = 2000;
 const webhook =
   "https://api.vercel.com/v1/integrations/deploy/prj_MQlxIz5zQP4GMibWntBaE9r3ws4B/zrVrtysmCF";
 const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
 let lapseInSeconds = 2;
+
 const exponentialRetryBackOff = async () => {
   while (lapseInSeconds < maxTimeInHours * 60 * 60) {
     try {
-      const { data, status, statusText } = await axios.post(webhook);
-
-      console.log({ data, lapseInSeconds, status, statusText });
-
-      if (status === 201) {
-        return 0;
-      }
+      const { data, status } = await axios.post(
+        webhook,
+        {},
+        { timeout: timeoutMs }
+      );
+      console.log({ data, status });
+      return 0;
     } catch (error) {
-      console.log(error);
-      return 1;
+      console.log(error.toJSON());
+      console.log(`Waiting ${lapseInSeconds} seconds before next retry...`);
+      await sleep(lapseInSeconds * 1000);
+      lapseInSeconds *= 2;
     }
-
-    await sleep(lapseInSeconds * 1000);
-    lapseInSeconds *= 2;
   }
 
+  console.log("Max time exceeded, no more retries left. Request failed!");
   return 1;
 };
 
