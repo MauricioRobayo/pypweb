d444ce891731df8d8e0d8b6333d5566594e3aa40
Mauricio Robayo <rfmajo@gmail.com>
2021-03-27 06:54:37 -0500

    refactor: remove the-moneytizer wrapper
    Signed-off-by: Mauricio Robayo <rfmajo@gmail.com>

---
+++ components/the-moneytizer/mega-banner.tsx
@@ -0,0 +1,71 @@
+import cn from "classnames";
+import useScript from "hooks/useScript";
+import { useRef } from "react";
+import styled, { css } from "styled-components";
+
+const isProduction = process.env.NODE_ENV === "production";
+const siteId = "71116";
+const baseUrl = "//ads.themoneytizer.com/s";
+
+export enum FormatType {
+  MEGABANNER = "1",
+  SKIN = "5",
+  RECOMMENDED_CONTENT = "16",
+}
+
+const classNames: Record<FormatType, string> = {
+  [FormatType.MEGABANNER]: "",
+  [FormatType.SKIN]: "",
+  [FormatType.RECOMMENDED_CONTENT]: "outbrain-tm",
+};
+
+const Banner = styled.div`
+  ${() =>
+    !isProduction &&
+    css`
+      align-items: center;
+      background-color: mistyrose;
+      border: 1px solid currentColor;
+      color: tomato;
+      display: flex;
+      font-weight: bold;
+      height: 90px;
+      justify-content: center;
+      max-width: 728px;
+      text-transform: uppercase;
+      width: 100%;
+    `};
+`;
+
+type Props = {
+  formatType: FormatType;
+  className?: string;
+};
+
+const TheMoneytizer = ({ className, formatType }: Props) => {
+  const div = useRef<HTMLDivElement>(null);
+
+  if (isProduction) {
+    useScript(div, `${baseUrl}/gen.js?type=${formatType}`);
+    useScript(
+      div,
+      `${baseUrl}/requestform.js?siteId=${siteId}&formatId=${formatType}`
+    );
+  }
+
+  return (
+    <Banner
+      ref={div}
+      className={cn(className, classNames[formatType])}
+      id={`${siteId}-${formatType}`}
+    >
+      {isProduction ? null : "MegaBanner"}
+    </Banner>
+  );
+};
+
+TheMoneytizer.defaultProps = {
+  className: "",
+};
+
+export default TheMoneytizer;
