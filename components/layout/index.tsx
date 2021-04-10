734fb010f84458d43900d4e477148c7e6b5c806c
Mauricio Robayo <rfmajo@gmail.com>
2021-04-10 11:38:45 -0500

    refactor: move layout styles to file
    Signed-off-by: Mauricio Robayo <rfmajo@gmail.com>

--- components/layout/index.tsx
+++ components/layout/index.tsx
@@ -1,155 +1,48 @@
-import TheMoneytizer from "components/the-moneytizer";
 import Vidverto from "components/vidverto";
 import Head from "next/head";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import { ReactNode } from "react";
-import styled, { css } from "styled-components";
-import { camouflageLink, responsivePaddingAround } from "styles/mixins";
 import { PypOption } from "types";
 import CTA from "../call-to-action";
 import PypDate from "../date";
 import Email from "../email";
 import LicensePlate from "../license-plate";
 import Select from "../select";
-
-const MegaBanner = styled(TheMoneytizer).attrs({
-  formatType: "MEGABANNER",
-})`
-  margin: 2rem auto 0;
-`;
-
-const RecommendedContent = styled(TheMoneytizer).attrs({
-  formatType: "RECOMMENDED_CONTENT",
-})`
-  margin: 2rem 0;
-`;
-
-const StyledLayout = styled.div`
-  align-items: center;
-  display: flex;
-  flex-direction: column;
-  min-height: 100vh;
-
-  header {
-    margin: auto;
-    max-width: ${({ theme }) => theme.maxWidth};
-    text-align: center;
-
-    h1 {
-      font-size: 2rem;
-      font-weight: bold;
-      margin: 1rem 0 1rem;
-    }
-
-    h2 {
-      font-size: 1.2rem;
-    }
-  }
-`;
-
-const Navbar = styled.div`
-  background-color: hsl(0, 0%, 98%);
-  border-bottom: 1px solid hsl(0, 0%, 88%);
-  padding: 0.5rem 1rem;
-  width: 100%;
-  nav {
-    align-items: center;
-    display: grid;
-    gap: 0.5rem;
-    grid-template-columns: auto 1fr;
-    margin: auto;
-    max-width: ${({ theme }) => theme.maxWidth};
-  }
-`;
-
-type PageProps = {
-  isHome: boolean;
-};
-const Page = styled.div<PageProps>`
-  display: flex;
-  flex-direction: column;
-  justify-content: ${({ isHome }) => (isHome ? "center" : "flex-start")};
-  width: ${({ isHome }) => (isHome ? "auto" : "100%")};
-  main {
-    align-items: center;
-    display: flex;
-    flex-direction: column;
-    padding: 0.5rem;
-  }
-  ${({ isHome }) =>
-    isHome &&
-    css`
-      flex: 1 1 100%;
-    `}
-`;
-
-const Aside = styled.aside`
-  ${responsivePaddingAround}
-
-  max-width: 720px;
-  h4 {
-    font-weight: bold;
-    margin: 1rem 0 0.5rem;
-  }
-  a {
-    color: ${({ theme }) => theme.colors.linkColor};
-  }
-  ol {
-    list-style-type: number;
-  }
-  ul {
-    list-style-type: disc;
-  }
-  ul,
-  ol {
-    list-style-position: outside;
-    margin-left: 1rem;
-  }
-  li,
-  p {
-    padding: 0.25rem 0;
-  }
-`;
-
-const Footer = styled.footer<PageProps>`
-  background-color: hsl(0, 0%, 98%);
-  border-top: 1px solid hsl(0, 0%, 88%);
-  font-size: 0.85rem;
-  margin-top: auto;
-  padding: 0.5rem 0 1rem;
-  text-align: center;
-  width: 100%;
-  p {
-    margin: 0.5rem;
-  }
-`;
-
-const Logo = styled.h2`
-  ${camouflageLink}
-`;
+import {
+  Aside,
+  Footer,
+  Logo,
+  MegaBanner,
+  Navbar,
+  Page,
+  RecommendedContent,
+  StyledLayout,
+} from "./styles";
 
 type LayoutProps = {
+  aside?: ReactNode;
   children: ReactNode;
   className?: string;
-  categoryName?: string;
   cityName?: string;
-  pypOptions: PypOption[];
-  isHome?: boolean;
-  aside?: ReactNode;
-  title?: string;
+  categoryName?: string;
   date: Date;
+  isHome?: boolean;
+  pypOptions: PypOption[];
+  selectedNumber?: number;
+  title?: string;
 };
 export default function Layout({
-  children,
-  categoryName = "",
-  cityName = "",
-  className = "",
-  isHome = false,
   aside = null,
-  pypOptions,
-  title = "Pico y placa hoy",
+  children,
+  categoryName,
+  cityName,
+  className = "",
   date,
+  isHome = false,
+  pypOptions,
+  selectedNumber,
+  title = "Pico y placa hoy",
 }: LayoutProps) {
   const router = useRouter();
 
@@ -159,6 +52,9 @@ export default function Layout({
   }
   if (cityName) {
     description += ` en ${cityName}`;
+  }
+  if (selectedNumber) {
+    description += ` placas ${selectedNumber}`;
   }
 
   return (
