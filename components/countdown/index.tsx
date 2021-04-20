186237add170fb611815bbb40f3101668381b747
Mauricio Robayo <rfmajo@gmail.com>
2021-04-19 20:33:48 -0500

    fix: countdown message remains after expired
    Signed-off-by: Mauricio Robayo <rfmajo@gmail.com>

--- components/countdown/index.tsx
+++ components/countdown/index.tsx
@@ -1,12 +1,22 @@
 import { useEffect, useState } from "react";
+import styled from "styled-components";
 import { formatDistance } from "./utils";
+
+const Message = styled.span`
+  color: ${({ theme }) => theme.colors.secondary};
+`;
 
 type CountDownProps = {
   endTime: Date;
   className?: string;
+  message?: string;
 };
 
-const Countdown = ({ className = "", endTime }: CountDownProps) => {
+const Countdown = ({
+  className = "",
+  endTime,
+  message = "",
+}: CountDownProps) => {
   const [countdown, setCountdown] = useState(formatDistance(endTime));
 
   useEffect(() => {
@@ -21,7 +31,12 @@ const Countdown = ({ className = "", endTime }: CountDownProps) => {
     return null;
   }
 
-  return <div className={className}>{countdown}</div>;
+  return (
+    <div className={className}>
+      {message === "" ? null : <Message>{message}</Message>}
+      <div className="countdown">{countdown}</div>
+    </div>
+  );
 };
 
 export default Countdown;
