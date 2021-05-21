01f8192d336dc628a736fbfa4424603d56257da4
Mauricio Robayo <rfmajo@gmail.com>
2021-05-21 07:30:24 -0500

    feat: add vidverto mobile script
    Signed-off-by: Mauricio Robayo <rfmajo@gmail.com>

--- components/ads/vidverto/index.tsx
+++ components/ads/vidverto/index.tsx
@@ -1,12 +1,54 @@
 /* eslint-disable react/no-danger */
 
 import AdPlaceholder from "components/ads/placeholder";
+import useDeviceDetect from "hooks/useDeviceDetect";
 import useScript from "hooks/useScript";
 import { useRef } from "react";
 
 const isProduction = process.env.NODE_ENV === "production";
-const id = "981cceae08e42e6301d86ae909b97156";
 const scriptUrl = "https://ad.vidverto.io/vidverto/js/aries/v1/invocation.js";
+const desktopId = "981cceae08e42e6301d86ae909b97156";
+const mobileId = "abf94b632c49d15ca7ced7d51dcb9cfc";
+
+const mobileScript = `(() => {
+  window.aries = window.aries || {};
+  window.aries.v1 = window.aries.v1 || {commands: []};
+
+  const aries = window.aries.v1;
+
+  aries.commands.push(() => {
+    const zoneId = '6668';
+    const anchor = document.getElementById('_vidverto-${mobileId}');
+
+    anchor.removeAttribute('id');
+
+    aries.mount(zoneId, anchor, {
+      width: 400,
+      height: 300,
+    });
+  });
+})();`;
+
+const desktopScript = `
+(() => {
+  window.aries = window.aries || {};
+  window.aries.v1 = window.aries.v1 || {commands: []};
+
+  const aries = window.aries.v1;
+
+  aries.commands.push(() => {
+    const zoneId = '6552';
+    const anchor = document.getElementById('_vidverto-${desktopId}');
+
+    anchor.removeAttribute('id');
+
+    aries.mount(zoneId, anchor, {
+      width: 720,
+      height: 405,
+    });
+  });
+})();
+`;
 
 type VidvertoProps = {
   className?: string;
@@ -14,6 +56,10 @@ type VidvertoProps = {
 const Vidverto = ({ className = "" }: VidvertoProps) => {
   const divRef = useRef(null);
   useScript(divRef, scriptUrl, true);
+  const { isMobile } = useDeviceDetect();
+
+  const script = isMobile ? mobileScript : desktopScript;
+  const id = isMobile ? mobileId : desktopId;
 
   if (isProduction) {
     return (
@@ -23,26 +69,7 @@ const Vidverto = ({ className = "" }: VidvertoProps) => {
         <div ref={divRef} id={`_vidverto-${id}`}>
           <script
             dangerouslySetInnerHTML={{
-              __html: `
-              (() => {
-                window.aries = window.aries || {};
-                window.aries.v1 = window.aries.v1 || {commands: []};
-
-                const aries = window.aries.v1;
-
-                aries.commands.push(() => {
-                  const zoneId = '6552';
-                  const anchor = document.getElementById('_vidverto-${id}');
-
-                  anchor.removeAttribute('id');
-
-                  aries.mount(zoneId, anchor, {
-                    width: 720,
-                    height: 405,
-                  });
-                });
-              })();
-            `,
+              __html: script,
             }}
           />
         </div>
