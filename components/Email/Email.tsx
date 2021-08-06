e582de6c50c561b6cf107d602625e6dbb1569513
Mauricio Robayo <rfmajo@gmail.com>
2021-08-06 10:26:07 -0500

    refactor: consolidate ObfuscatedEmail into Email component
    Signed-off-by: Mauricio Robayo <rfmajo@gmail.com>

--- components/Email/Email.tsx
+++ components/Email/Email.tsx
@@ -1,5 +1,18 @@
 import { useState } from "react";
-import ObfuscatedEmail from "./ObfuscatedEmail";
+import styled, { css } from "styled-components";
+
+type SegmentWrapperProps = {
+  first: boolean;
+};
+const SegmentWrapper = styled.span<SegmentWrapperProps>`
+  ${({ first }) =>
+    first &&
+    css`
+      &::after {
+        content: "\x00040";
+      }
+    `}
+`;
 
 type EmailProps = {
   className?: string;
@@ -13,8 +26,19 @@ export default function Email({ className, email }: EmailProps) {
   }
 
   return (
-    <span className={className} onFocus={handleHover} onMouseOver={handleHover}>
-      {hovered ? email : <ObfuscatedEmail email={email} />}
-    </span>
+    <a
+      className={className}
+      href={hovered ? `mailto:${email}` : "#"}
+      onFocus={handleHover}
+      onMouseOver={handleHover}
+    >
+      {hovered
+        ? email
+        : email.split("@").map((emailSegment, index) => (
+            <SegmentWrapper key={emailSegment} first={index === 0}>
+              {emailSegment}
+            </SegmentWrapper>
+          ))}
+    </a>
   );
 }
