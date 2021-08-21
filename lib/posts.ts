2a25b7a256eaf75d71b0955024a5e797046035db
Mauricio Robayo <rfmajo@gmail.com>
2021-08-21 18:34:51 -0500

    chore: rename _posts dir to posts
    Signed-off-by: Mauricio Robayo <rfmajo@gmail.com>

--- lib/posts.ts
+++ lib/posts.ts
@@ -2,7 +2,7 @@ import fs from "fs";
 import matter from "gray-matter";
 import { join } from "path";
 
-const postsDirectory = join(process.cwd(), "_posts");
+const postsDirectory = join(process.cwd(), "posts");
 
 export default function getPostBySlug(slug: string) {
   const realSlug = slug.replace(/\.md$/, "");
