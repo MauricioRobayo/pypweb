ca61229300eb2f65b756d96c55be23e485438d08
Mauricio Robayo <rfmajo@gmail.com>
2021-08-24 16:53:47 -0500

    feat: add Fine component
    Signed-off-by: Mauricio Robayo <rfmajo@gmail.com>

---
+++ components/Fine/Fine.tsx
@@ -0,0 +1,32 @@
+export function Fine() {
+  return (
+    <>
+      <h4>Sanciones</h4>
+      <p>
+        Los infractores a lo dispuesto serán sancionados con multa equivalente a
+        quince (15) salarios mínimos diarios legales vigentes e inmovilización
+        del vehículo, según lo consagrado por el
+        <a href="http://www.secretariasenado.gov.co/senado/basedoc/ley_0769_2002_pr003.html#131">
+          artículo 131 literal C numeral 14 de la Ley 769 de 2002
+        </a>
+        modificado por el{" "}
+        <a href="http://www.secretariasenado.gov.co/senado/basedoc/ley_1383_2010.html#21">
+          artículo 21 de la Ley 1383 de 2010
+        </a>
+        :
+      </p>
+      <blockquote>
+        <p>
+          C. Será sancionado con multa equivalente a quince (15) salarios
+          mínimos legales diarios vigentes (SMLDV) el conductor y/o propietario
+          de un vehículo automotor que incurra en cualquiera de las siguientes
+          infracciones:
+        </p>
+        <p>
+          C.14. Transitar por sitios restringidos o en horas prohibidas por la
+          autoridad competente. Además, el vehículo será inmovilizado.
+        </p>
+      </blockquote>
+    </>
+  );
+}
---
+++ components/Fine/index.ts
@@ -0,0 +1,1 @@
+export { Fine } from "./Fine";
