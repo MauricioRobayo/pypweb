d02b02d84b9e78ec4c5460ab729953566f996fa5
Mauricio Robayo <rfmajo@gmail.com>
2021-08-24 17:12:04 -0500

    chore: add styles
    Signed-off-by: Mauricio Robayo <rfmajo@gmail.com>

--- components/Fine/Fine.tsx
+++ components/Fine/Fine.tsx
@@ -1,32 +1,45 @@
+import styled from "styled-components";
+
 export function Fine() {
+  const Quote = styled.blockquote`
+    background-color: ${({ theme }) => theme.colors.warningLighter};
+    border-radius: 0.5rem;
+    font-style: italic;
+    margin: 1rem 1.5rem;
+    padding: 1rem 1.5rem;
+    p {
+      margin: 0;
+    }
+  `;
+
   return (
     <>
       <h4>Sanciones</h4>
       <p>
-        Los infractores a lo dispuesto serán sancionados con multa equivalente a
-        quince (15) salarios mínimos diarios legales vigentes e inmovilización
-        del vehículo, según lo consagrado por el
+        Los infractores a lo dispuesto serán sancionados según lo consagrado por
+        el{" "}
         <a href="http://www.secretariasenado.gov.co/senado/basedoc/ley_0769_2002_pr003.html#131">
           artículo 131 literal C numeral 14 de la Ley 769 de 2002
-        </a>
+        </a>{" "}
         modificado por el{" "}
         <a href="http://www.secretariasenado.gov.co/senado/basedoc/ley_1383_2010.html#21">
           artículo 21 de la Ley 1383 de 2010
         </a>
         :
       </p>
-      <blockquote>
+      <Quote>
         <p>
-          C. Será sancionado con multa equivalente a quince (15) salarios
-          mínimos legales diarios vigentes (SMLDV) el conductor y/o propietario
-          de un vehículo automotor que incurra en cualquiera de las siguientes
-          infracciones:
+          <small>C.</small> Será sancionado con multa equivalente a quince (15)
+          salarios mínimos legales diarios vigentes (SMLDV) el conductor y/o
+          propietario de un vehículo automotor que incurra en cualquiera de las
+          siguientes infracciones:
         </p>
         <p>
-          C.14. Transitar por sitios restringidos o en horas prohibidas por la
-          autoridad competente. Además, el vehículo será inmovilizado.
+          <small>C.14.</small> Transitar por sitios restringidos o en horas
+          prohibidas por la autoridad competente. Además, el vehículo será
+          inmovilizado.
         </p>
-      </blockquote>
+      </Quote>
     </>
   );
 }
