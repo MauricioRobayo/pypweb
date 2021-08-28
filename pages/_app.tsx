50ec5d93cd564c973bfa91c38e2a9aea19b0decc
Mauricio Robayo <rfmajo@gmail.com>
2021-08-28 11:00:17 -0500

    feat: send web vitals to google analytics
    Signed-off-by: Mauricio Robayo <rfmajo@gmail.com>

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,8 @@
 import { Layout } from "components/Layout";
 import { NextPage } from "next";
 import { DefaultSeo } from "next-seo";
-import { AppProps } from "next/app";
-import { GoogleAnalytics, usePagesViews } from "nextjs-google-analytics";
+import { AppProps, NextWebVitalsMetric } from "next/app";
+import { event, GoogleAnalytics, usePagesViews } from "nextjs-google-analytics";
 import NextNprogress from "nextjs-progressbar";
 import React, { ReactNode } from "react";
 import { ThemeProvider } from "styled-components";
@@ -18,6 +18,20 @@ type Page<P = {}> = NextPage<P> & {
 type CustomAppProps = AppProps & {
   Component: Page;
 };
+
+export function reportWebVitals({
+  id,
+  name,
+  label,
+  value,
+}: NextWebVitalsMetric) {
+  event(name, {
+    category: label === "web-vital" ? "Web Vitals" : "Next.js custom metric",
+    value: Math.round(name === "CLS" ? value * 1000 : value), // values must be integers
+    label: id, // id unique to current page load
+    nonInteraction: true, // avoids affecting bounce rate.
+  });
+}
 
 const App = ({ Component, pageProps }: CustomAppProps) => {
   const getLayout =
