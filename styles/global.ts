ebd7d98472229ee6558adade074ee5fd2f024685
Mauricio Robayo <rfmajo@gmail.com>
2021-09-26 19:53:38 -0500

    fix(#325): correctly prioritize fonts
    Signed-off-by: Mauricio Robayo <rfmajo@gmail.com>

--- styles/global.ts
+++ styles/global.ts
@@ -7,13 +7,11 @@ const GlobalStyle = createGlobalStyle`
     font-family: "LicensePlate";
     font-style: normal;
     font-weight: normal;
-    src: url("/fonts/LicensePlate.eot");
-    src: url("/fonts/LicensePlate.eot?#iefix") format("embedded-opentype"),
-      url("/fonts/LicensePlate.otf") format("opentype"),
-      url("/fonts/LicensePlate.svg") format("svg"),
-      url("/fonts/LicensePlate.ttf") format("truetype"),
+    src: local("License Plate"),
+      url("/fonts/LicensePlate.woff2") format("woff2"),
       url("/fonts/LicensePlate.woff") format("woff"),
-      url("/fonts/LicensePlate.woff2") format("woff2");
+      url("/fonts/LicensePlate.ttf") format("truetype"),
+      url("/fonts/LicensePlate.eot") format("embedded-opentype") ;
   }
   *, *::before, *::after {
   box-sizing: border-box;
