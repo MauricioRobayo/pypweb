074544334cde985130d16fedce35e490d70672ea
Mauricio Robayo <rfmajo@gmail.com>
2021-09-29 11:58:08 -0500

    fix(#325): inline category icons svgs
    Signed-off-by: Mauricio Robayo <rfmajo@gmail.com>

--- components/Icon/Icon.tsx
+++ components/Icon/Icon.tsx
@@ -1,19 +1,24 @@
 import { CategoryName } from "@mauriciorobayo/pyptron";
-import { Emoji } from "components/Emoji";
-import React from "react";
+import styled from "styled-components";
+import BusIcon from "svg-emojis/twemoji/1f68c.svg";
+import MinivanIcon from "svg-emojis/twemoji/1f690.svg";
+import TaxiIcon from "svg-emojis/twemoji/1f695.svg";
+import CarIcon from "svg-emojis/twemoji/1f697.svg";
+import TruckIcon from "svg-emojis/twemoji/1f69b.svg";
+import MotoIcon from "svg-emojis/twemoji/1f6f5.svg";
 
-const iconsMap: Record<CategoryName, string> = {
-  [CategoryName.MOTOS]: "🛵",
-  [CategoryName.MOTOCARROS]: "🛵",
-  [CategoryName.PARTICULARES]: "🚗",
-  [CategoryName.SERVICIO_DE_TRANSPORTE_ESPECIAL]: "🚐",
-  [CategoryName.TAXIS]: "🚕",
-  [CategoryName.TRANSPORTE_DE_CARGA]: "🚛",
-  [CategoryName.TRANSPORTE_PUBLICO_COLECTIVO]: "🚌",
-  [CategoryName.CARGA_MAS_DE_20_ANOS_DE_EDAD]: "🚛",
-  [CategoryName.CARGA_PESO_MAX_SUPERIOR_A_3500KG]: "🚛",
-  [CategoryName.CARGA_PESO_MAX_SUPERIOR_A_8500KG]: "🚛",
-  [CategoryName.TRANSPORTE_DE_CARGA_MENOR_A_1500KG]: "🚛",
+const iconsMap: Record<CategoryName, any> = {
+  [CategoryName.MOTOS]: MotoIcon,
+  [CategoryName.MOTOCARROS]: MotoIcon,
+  [CategoryName.PARTICULARES]: CarIcon,
+  [CategoryName.SERVICIO_DE_TRANSPORTE_ESPECIAL]: MinivanIcon,
+  [CategoryName.TAXIS]: TaxiIcon,
+  [CategoryName.TRANSPORTE_DE_CARGA]: TruckIcon,
+  [CategoryName.TRANSPORTE_PUBLICO_COLECTIVO]: BusIcon,
+  [CategoryName.CARGA_MAS_DE_20_ANOS_DE_EDAD]: TruckIcon,
+  [CategoryName.CARGA_PESO_MAX_SUPERIOR_A_3500KG]: TruckIcon,
+  [CategoryName.CARGA_PESO_MAX_SUPERIOR_A_8500KG]: TruckIcon,
+  [CategoryName.TRANSPORTE_DE_CARGA_MENOR_A_1500KG]: TruckIcon,
 };
 
 type IconProps = {
@@ -21,10 +26,12 @@ type IconProps = {
   iconName: CategoryName;
 };
 
-const Icon = ({ iconName, className = "" }: IconProps) => (
-  <span aria-label="iconName" className={className} role="img">
-    <Emoji emoji={iconsMap[iconName]} />
-  </span>
-);
+function Icon({ iconName, className }: IconProps) {
+  const SVGIcon = iconsMap[iconName];
+  return <SVGIcon className={className} />;
+}
 
-export default Icon;
+export default styled(Icon)`
+  height: 1em;
+  width: 1em;
+`;
