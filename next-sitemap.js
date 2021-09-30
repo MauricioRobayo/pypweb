8ea00d3ab1d8f1528f89f74b26ab1f797e56affa
Mauricio Robayo <rfmajo@gmail.com>
2021-09-30 17:02:50 -0500

    fix(#351): sitemap home page date is update daily
    Signed-off-by: Mauricio Robayo <rfmajo@gmail.com>

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,35 +1,4 @@
 module.exports = {
   siteUrl: "https://www.pyphoy.com",
   generateRobotsTxt: true,
-  transform: async (config, path) => {
-    if (path === "/") {
-      return {
-        loc: path,
-        changefreq: "yearly",
-        priority: 1,
-      };
-    }
-    if (path.split("/").length === 2) {
-      return {
-        loc: path,
-        changefreq: "daily",
-        priority: 0.8,
-        lastmod: new Date().toISOString(),
-      };
-    }
-    if (path.split("/").length === 3) {
-      return {
-        loc: path,
-        changefreq: "daily",
-        priority: 0.9,
-        lastmod: new Date().toISOString(),
-      };
-    }
-    return {
-      loc: path,
-      changefreq: "daily",
-      priority: 0.7,
-      lastmod: new Date().toISOString(),
-    };
-  },
 };
