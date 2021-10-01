a26f015f330b4e122a273d1d91f7cae786afcef5
Mauricio Robayo <rfmajo@gmail.com>
2021-10-01 15:51:30 -0500

    feat(#290): add auto-link to Fine component
    Signed-off-by: Mauricio Robayo <rfmajo@gmail.com>

--- components/Fine/Fine.tsx
+++ components/Fine/Fine.tsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import LinkIcon from "svg-emojis/twemoji/1f517.svg";
 
 const Quote = styled.blockquote`
   background-color: ${({ theme }) => theme.colors.secondaryLighter};
@@ -18,7 +19,12 @@ const Quote = styled.blockquote`
 export function Fine() {
   return (
     <>
-      <h4>Sanciones</h4>
+      <h4 id="sanciones">
+        Sanciones
+        <a aria-hidden="true" tabIndex={-1} href="#sanciones">
+          <LinkIcon />
+        </a>
+      </h4>
       <p>
         Los infractores a lo dispuesto serán sancionados según lo consagrado por
         el{" "}
