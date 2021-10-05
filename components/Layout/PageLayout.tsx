34c125ec0c34c66f6227614382b21fbf5fed0532
Mauricio Robayo <rfmajo@gmail.com>
2021-10-05 06:54:01 -0500

    fix(#377): include categories path in cities array
    Signed-off-by: Mauricio Robayo <rfmajo@gmail.com>

--- components/Layout/PageLayout.tsx
+++ components/Layout/PageLayout.tsx
@@ -2,6 +2,7 @@ import { FixedHeader } from "components/FixedHeader";
 import { Footer } from "components/Footer";
 import { Navbar } from "components/Navbar";
 import type { CitiesList } from "lib/cities";
+import { useRouter } from "next/router";
 import React, { ReactNode } from "react";
 import styled from "styled-components";
 
@@ -17,10 +18,15 @@ type LayoutProps = {
   cities: CitiesList;
 };
 export default function Layout({ children, cities }: LayoutProps) {
+  const { asPath } = useRouter();
+
+  const pathSegments = asPath.split("/");
+
   return (
     <StyledLayout>
       <Navbar cities={cities} />
       <FixedHeader />
+      {pathSegments.length > 1 ? asPath : null}
       {children}
       <Footer />
     </StyledLayout>
