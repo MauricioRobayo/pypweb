05338c4f1a9104d49bd663e8ac7aca5a6c077bcc
Mauricio Robayo <rfmajo@gmail.com>
2021-10-06 09:50:46 -0500

    381/Breadcrumbs-enhancements (#382)
    * fix(#381): add title to link items
    
    Signed-off-by: Mauricio Robayo <rfmajo@gmail.com>
    
    * fix(#381): add microdate to breadcrumb items
    
    Signed-off-by: Mauricio Robayo <rfmajo@gmail.com>
    
    * fix(#381): microdate/semantic markup
    
    Signed-off-by: Mauricio Robayo <rfmajo@gmail.com>
    
    * fix(#381): microdate/semantic markup
    
    Signed-off-by: Mauricio Robayo <rfmajo@gmail.com>

--- components/Breadcrumbs/Breadcrumbs.tsx
+++ components/Breadcrumbs/Breadcrumbs.tsx
@@ -1,6 +1,6 @@
 import { Select } from "components/Select";
 import Link from "next/link";
-import React, { Fragment } from "react";
+import React from "react";
 import styled from "styled-components";
 import { responsiveWidth } from "styles/mixins";
 
@@ -21,21 +21,36 @@ const StyledSelect = styled(Select)`
     padding: 0 1.25em 0 0;
   }
 `;
-const Wrapper = styled.nav`
+const Wrapper = styled.ol`
   ${responsiveWidth}
 
   align-items: center;
   display: flex;
   font-size: ${({ theme }) => theme.font.size.small};
+  margin: 0;
+  padding: 0;
   a {
     text-decoration: none;
   }
 `;
+
+const BreadcrumbItemContainer = styled.li.attrs({
+  itemProp: "itemListElement",
+  itemScope: true,
+  itemType: "https://schema.org/ListItem",
+})`
+  display: flex;
+`;
+
 const BreadcrumbItem = styled.div`
   max-width: 22ch;
   overflow: hidden;
   text-overflow: ellipsis;
   white-space: nowrap;
+`;
+
+const ItemSeparator = styled.span`
+  margin: 0 0.25em;
 `;
 
 type Props = {
@@ -49,28 +64,36 @@ function Breadcrumbs({ path, className = "" }: Props) {
   }
 
   return (
-    <Wrapper className={className}>
-      {path.map((item) => {
+    <Wrapper
+      className={className}
+      itemScope
+      itemType="https://schema.org/BreadcrumbList"
+    >
+      {path.map((item, i) => {
         if (isPathSegment(item)) {
           return (
-            <Fragment key={item.path}>
+            <BreadcrumbItemContainer key={item.path}>
               <BreadcrumbItem>
                 <Link href={item.path}>
-                  <a>{item.name}</a>
+                  <a title={item.path} itemProp="item">
+                    <span itemProp="name">{item.name}</span>
+                    <meta itemProp="position" content={`${i + 1}`} />
+                  </a>
                 </Link>
               </BreadcrumbItem>
-              <span> &gt; </span>
-            </Fragment>
+              <ItemSeparator>&gt;</ItemSeparator>
+            </BreadcrumbItemContainer>
           );
         }
 
         return (
-          <StyledSelect
-            name="select"
-            key={item.selected}
-            options={item.options}
-            selected={item.selected}
-          />
+          <BreadcrumbItem key={item.selected}>
+            <StyledSelect
+              name="select"
+              options={item.options}
+              selected={item.selected}
+            />
+          </BreadcrumbItem>
         );
       })}
     </Wrapper>
