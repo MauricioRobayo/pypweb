ca0d9a729e95ac27df5697f5247e63f7774036fd
Mauricio Robayo <rfmajo@gmail.com>
2021-10-08 13:45:17 -0500

    chore(#392): remove the moneytizer components
    Signed-off-by: Mauricio Robayo <rfmajo@gmail.com>

--- components/Ads/index.ts
+++ components/Ads/index.ts
@@ -3,5 +3,4 @@ export { ConsentBanner } from "./ConsentBanner";
 export { Placeholder } from "./Placeholder";
 export { SelfAd } from "./SelfAd";
 export { BelowArticleThumbnails, Taboola } from "./Taboola";
-export { default as TheMoneytizer } from "./TheMoneytizer";
 export { default as Vidverto } from "./Vidverto";
