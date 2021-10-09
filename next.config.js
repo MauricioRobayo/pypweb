dbd22203ccb2b6db439ec6f766ede760e4be16e3
Mauricio Robayo <rfmajo@gmail.com>
2021-10-09 13:53:31 -0500

    chore(#396): simplify redirects
    Signed-off-by: Mauricio Robayo <rfmajo@gmail.com>

--- next.config.js
+++ next.config.js
@@ -2,59 +2,34 @@ module.exports = {
   async redirects() {
     return [
       {
-        source: "/:city/:category/placa/:number",
+        source: "/:city/:category/placa/:number*",
+        destination: "/:city/:category/:number*",
         permanent: true,
-        destination: "/:city/:category/:number",
       },
       {
-        source: "/popayan/transporte-de-carga",
+        source: "/lorica/:category*",
+        destination: "/santa-cruz-de-lorica/:category*",
         permanent: true,
-        destination: "/popayan/transporte-de-carga-menor-a-1.500kg",
       },
       {
-        source: "/lorica",
+        source: "/popayan/transporte-de-carga/:number*",
+        destination: "/popayan/transporte-de-carga-menor-a-1500kg/:number*",
         permanent: true,
-        destination: "/santa-cruz-de-lorica",
-      },
-      {
-        source: "/lorica/:category",
-        permanent: true,
-        destination: "/santa-cruz-de-lorica/:category",
-      },
-      {
-        source: "/lorica/:category/:number",
-        permanent: true,
-        destination: "/santa-cruz-de-lorica/:category/:number",
-      },
-      {
-        source: "/popayan/transporte-de-carga-menor-a-1.500kg",
-        permanent: true,
-        destination: "/popayan/transporte-de-carga-menor-a-1500kg",
-      },
-      {
-        source: "/bogota/carga-peso-max.-superior-a-8.500kg",
-        permanent: true,
-        destination: "/bogota/carga-peso-max-superior-a-8500kg",
-      },
-      {
-        source: "/bogota/carga-peso-max.-superior-a-3.500kg",
-        permanent: true,
-        destination: "/bogota/carga-peso-max-superior-a-3500kg",
       },
       {
         source: "/popayan/transporte-de-carga-menor-a-1.500kg/:number*",
-        permanent: true,
         destination: "/popayan/transporte-de-carga-menor-a-1500kg/:number*",
+        permanent: true,
       },
       {
         source: "/bogota/carga-peso-max.-superior-a-8.500kg/:number*",
-        permanent: true,
         destination: "/bogota/carga-peso-max-superior-a-8500kg/:number*",
+        permanent: true,
       },
       {
         source: "/bogota/carga-peso-max.-superior-a-3.500kg/:number*",
-        permanent: true,
         destination: "/bogota/carga-peso-max-superior-a-3500kg/:number*",
+        permanent: true,
       },
     ];
   },
