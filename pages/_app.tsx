8aa1ef20022abbb1811ba9944e35151235074be2
Mauricio Robayo <rfmajo@gmail.com>
2021-10-08 20:11:12 -0500

    fix(#407): back to default loading strategy
    Signed-off-by: Mauricio Robayo <rfmajo@gmail.com>

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -41,7 +41,7 @@ const App = ({ Component, pageProps }: AppPropsWithLayout) => {
 
   return (
     <>
-      <GoogleAnalytics strategy="lazyOnload" />
+      <GoogleAnalytics />
       <ConsentBanner />
       <AdSense />
       <Taboola />
