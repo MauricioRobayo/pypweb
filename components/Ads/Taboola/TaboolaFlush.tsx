fe1ffacc9dc8ec25dc37b78dab0ae9049b49b9bb
Mauricio Robayo <rfmajo@gmail.com>
2021-10-12 10:29:22 -0500

    415/Remove-Taboola-debuggin-condition (#418)
    * fix(#415): use query to show dev taboola ads
    
    Signed-off-by: Mauricio Robayo <rfmajo@gmail.com>
    
    * fix(#415): remove viderto publishers from ads.txt
    
    Signed-off-by: Mauricio Robayo <rfmajo@gmail.com>
    
    * fix(#415): restore taboola flush
    
    Signed-off-by: Mauricio Robayo <rfmajo@gmail.com>
    
    * chore(#415): remove unused vidverto component
    
    Signed-off-by: Mauricio Robayo <rfmajo@gmail.com>

--- components/Ads/Taboola/TaboolaFlush.tsx
+++ components/Ads/Taboola/TaboolaFlush.tsx
@@ -1,15 +1,18 @@
 import { shouldShowAds } from "lib/utils";
+import { useRouter } from "next/router";
 import Script from "next/script";
 
 export default function TaboolaFlush() {
-  if (!shouldShowAds && Math.random() > 1) {
-    return null;
+  const { query } = useRouter();
+
+  if (shouldShowAds || query.taboola_sim_domain) {
+    return (
+      <Script id="taboola-flush" strategy="lazyOnload">
+        {`window._taboola = window._taboola || [];
+        _taboola.push({flush: true});`}
+      </Script>
+    );
   }
 
-  return (
-    <Script id="taboola-flush" strategy="lazyOnload">
-      {`window._taboola = window._taboola || [];
-      _taboola.push({flush: true});`}
-    </Script>
-  );
+  return null;
 }
--- components/Ads/index.ts
+++ components/Ads/index.ts
@@ -3,4 +3,3 @@ export { ConsentBanner } from "./ConsentBanner";
 export { Placeholder } from "./Placeholder";
 export { SelfAd } from "./SelfAd";
 export { BelowArticleThumbnails, Taboola } from "./Taboola";
-export { default as Vidverto } from "./Vidverto";
