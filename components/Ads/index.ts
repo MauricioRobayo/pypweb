ff9089d4053885cf77d75a19c19fb0350faaad51
Mauricio Robayo <rfmajo@gmail.com>
2021-10-13 14:57:11 -0500

    fix(#427): restore vidverto component
    Signed-off-by: Mauricio Robayo <rfmajo@gmail.com>

--- components/Ads/index.ts
+++ components/Ads/index.ts
@@ -2,3 +2,4 @@ export { AdSense } from "./AdSense";
 export { ConsentBanner } from "./ConsentBanner";
 export { Placeholder } from "./Placeholder";
 export { SelfAd } from "./SelfAd";
+export { Vidverto } from "./Vidverto";
