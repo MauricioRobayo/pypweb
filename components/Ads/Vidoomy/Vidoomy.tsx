371da9b8c562a684e9c6109e62b5806e5a9418f0
Mauricio Robayo <rfmajo@gmail.com>
2021-10-13 16:07:31 -0500

    chore(#426): add protocol to script src
    Signed-off-by: Mauricio Robayo <rfmajo@gmail.com>

--- components/Ads/Vidoomy/Vidoomy.tsx
+++ components/Ads/Vidoomy/Vidoomy.tsx
@@ -3,7 +3,7 @@ import Script from "next/script";
 export default function Vidoomy() {
   return (
     <Script
-      src="//ads.vidoomy.com/pyphoycom_18006.js"
+      src="https://ads.vidoomy.com/pyphoycom_18006.js"
       strategy="beforeInteractive"
     />
   );
