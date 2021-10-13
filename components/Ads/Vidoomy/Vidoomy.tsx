aafae7dca96716c972cd02129dce02e4edda317b
Mauricio Robayo <rfmajo@gmail.com>
2021-10-13 16:05:03 -0500

    chore(#426): add Vidoomy verification script
    Signed-off-by: Mauricio Robayo <rfmajo@gmail.com>

---
+++ components/Ads/Vidoomy/Vidoomy.tsx
@@ -0,0 +1,10 @@
+import Script from "next/script";
+
+export default function Vidoomy() {
+  return (
+    <Script
+      src="//ads.vidoomy.com/pyphoycom_18006.js"
+      strategy="beforeInteractive"
+    />
+  );
+}
