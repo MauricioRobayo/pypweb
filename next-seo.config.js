e754b94a7c49567b8300227d8cc2551d6ff7e98d
Mauricio Robayo <rfmajo@gmail.com>
2021-10-21 11:47:44 -0500

    fix(#438): remove titleTemplate
    Signed-off-by: Mauricio Robayo <rfmajo@gmail.com>

--- next-seo.config.js
+++ next-seo.config.js
@@ -65,5 +65,4 @@ export const defaultConfig = {
     type: "website",
     updated_time: `${new Date().toISOString().slice(0, 10)}T00:00:00.000-05:00`,
   },
-  titleTemplate: `%s | ${siteName}`,
 };
