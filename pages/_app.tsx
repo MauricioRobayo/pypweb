5adab9d401b7875b7fb02253fac9f6f2ec50b2c7
Mauricio Robayo <rfmajo@gmail.com>
2021-11-21 20:51:16 -0500

    feat(#480): femove Taboola
    Signed-off-by: Mauricio Robayo <rfmajo@gmail.com>

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,4 @@
-import {
-  AdPlayerPro,
-  AdSense,
-  ConsentBanner,
-  Optima,
-  Taboola,
-} from "components/Ads";
+import { AdPlayerPro, AdSense, ConsentBanner, Optima } from "components/Ads";
 import { NextPage } from "next";
 import { DefaultSeo } from "next-seo";
 import { AppProps, NextWebVitalsMetric } from "next/app";
@@ -49,7 +43,6 @@ const App = ({ Component, pageProps }: AppPropsWithLayout) => {
       <GoogleAnalytics />
       <ConsentBanner />
       <AdSense />
-      <Taboola />
       <Optima />
       <AdPlayerPro />
       <DefaultSeo {...defaultConfig} />
